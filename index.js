a4d506425468c7f0487345f2043bf012ee93a775
Laura Berge <lauraeberge@gmail.com>
2020-07-12 18:10:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 // Your code here
 class Cat {
+  constructor(name){
+    self.name = name
+  }
+  
   speak(){
-    
+    return `${self.name} says meow!`
   }
 }
 
 class Dog {
+  constructor(name){
+    self.name = name
+  }
+  
   speak(){
-    
+    return `${self.name} says woof!`
   }
 }
 
 class Bird {
+  constructor(name){
+    self.name = name
+  }
+  
   speak(){
-    
+    return `${self.name} says squawk!`
   }
 }
